673ebefb75d599baf83bb62ace876ab415a2b929
Dominik Kaiser <dkaiser@1-C-9.42heilbronn.de>
2024-03-15 13:06:34 +0100

    Update includes

--- ft_printf.h
+++ ft_printf.h
@@ -6,15 +6,16 @@
 /*   By: dkaiser <dkaiser@student.42heilbronn.de    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/12 18:19:47 by dkaiser           #+#    #+#             */
-/*   Updated: 2024/03/15 12:40:58 by dkaiser          ###   ########.fr       */
+/*   Updated: 2024/03/15 13:04:52 by dkaiser          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef FT_PRINTF_H
 # define FT_PRINTF_H
 
-# include "libft/libft.h"
+//# include "libft/libft.h"
 # include <stdarg.h>
+# include <unistd.h>
 
 int	ft_printf(const char *fmt, ...);
 int	ft_printnbr(int nbr);
--- src/ft_printf.c
+++ src/ft_printf.c
@@ -6,12 +6,11 @@
 /*   By: dkaiser <dkaiser@student.42heilbronn.de    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/12 18:18:59 by dkaiser           #+#    #+#             */
-/*   Updated: 2024/03/15 12:40:47 by dkaiser          ###   ########.fr       */
+/*   Updated: 2024/03/15 13:05:57 by dkaiser          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../ft_printf.h"
-#include <stdarg.h>
 
 static int	print_char(char c)
 {
